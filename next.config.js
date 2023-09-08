d712e9cbd746e8004ec195bfb3a67cbe0cdf6cf6
gwonyejun <kyjuni1059@gmail.com>
2023-09-08 17:48:27 +0900

    주석 제거

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 const withPlugins = require('next-compose-plugins');
 const runtimeCaching = require('next-pwa/cache');
 const withPWA = require('next-pwa');
-// const withPWA = require('next-pwa')({
-//   pwa: {
-//     dest: 'public',
-//     register: true,
-//     skipWaiting: true,
-//     runtimeCaching: require('next-pwa/cache'),
-//     disable: process.env.NODE_ENV === 'development',
-//   },
-// });
 
 const nextConfig = withPWA({
   experimental: {
