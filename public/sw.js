1fd339a2e2f9265ddda33b9e08df28d9b88905d8
gwonyejun <kyjuni1059@gmail.com>
2023-09-08 16:46:35 +0900

    드디어 됐다..

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+// sw.js
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  var cacheWhitelist = ['pwa-task-manager'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
